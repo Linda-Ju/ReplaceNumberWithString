7253f23e0695549740966cf1aa8a705d7d1f34ff
Linda-Ju <linda.junkina@gmail.com>
2021-07-02 15:46:08 +0300

    HOMEWORK Replace number by string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String fizz = "Fizz";
-        String buzz = "Buzz";
-        String fizzbuzz = "FizzBuzz";
-      int j = 0;
         for (int i = 1; i < 51; i++) {
             if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 7 == 0) {
-//                System.out.println("Buzz");
+                System.out.println("Buzz");
             } else if (i % 3 == 0 && i % 7 == 0) {
- //               System.out.println("FizzBuzz");
+                System.out.println("FizzBuzz");
+            }else{
+                System.out.println(i);
             }
         }
     }
